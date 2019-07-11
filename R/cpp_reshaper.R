ae5d956169d33ae04d1f1f96b5120bdea7d5df37
Christian Testa <testa101@gmail.com>
2019-07-11 19:59:16 -0400

    Put the cpp_reshaper into a construct_cpp_reshaper function

--- R/cpp_reshaper.R
+++ R/cpp_reshaper.R
@@ -1,6 +1,8 @@
 
-cpp_reshaper <- cxxfunction(
-	signature(ResTab='numeric', ResTabus='numeric', ResTabfb='numeric', res_tab2 = 'numeric'),
-	plugin='Rcpp',
-	body=readr::read_file(
-		system.file('inline_cpp/format_restab2.cpp', package='tabus')))
+construct_cpp_reshaper <- function() {
+	cxxfunction(
+		signature(ResTab='numeric', ResTabus='numeric', ResTabfb='numeric', res_tab2 = 'numeric'),
+		plugin='Rcpp',
+		body=readr::read_file(
+			system.file('inline_cpp/format_restab2.cpp', package='tabus')))
+}
